4dacc9c8b4289d37b246484a809b4167779ed44c
Lord-Jaraxxus <ym970812@naver.com>
2022-08-04 22:39:01 +0900

    8/4 RBT 추가 완성
    집에서 들으니까 집중을 1도 못해서 수업시간에 했던 STL 관련 내용은 없음
    
    삭제는... 해야.. 하나..?

--- tree_2_RBT/tree_2_RBT.cpp
+++ tree_2_RBT/tree_2_RBT.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include "Node.h"
 
-int g_iData[10] = { 50,22,66,35,43,59,42,78,63,14 };
+//int g_iData[10] = { 50,22,66,35,43,59,42,78,63,14 };
+int g_iData[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int g_iCount = 0;
 int g_iFlag = 0;
 
@@ -40,37 +41,65 @@ Node* AddNode_BST(Node* pParent)
     return pParent;
 }
 
-int GetColor(Node* node) 
+int GetColor(Node* node)
 {
     if (node == nullptr) return BLACK;
     else return node->m_iColor;
 }
 
-void LeftRotate(Node* node) 
+void LeftRotate(Node* node)
 {
-}
-
-void RightRotate(Node* node) 
-{
-    Node* pPr = node->m_pParent; 
-    Node* pGpr = pPr->m_pParent;
+    Node* pPr = node->m_pParent;
 
     //앞으로 연결이 끊길 노드들을 미리 세이브해둠
-    Node* pPrLc = pPr->m_pChild[0]; // pPr의 원래 왼쪽 자식 (p Parent Left chid)
-    Node* pGGpr = pGpr->m_pParent; // 조부모의 부모, 즉 증조부 (p Grand-Grand parent)
+    Node* pLc = node->m_pChild[0]; // 노드의 원래 왼쪽 자식 (p Left chid)
+    Node* pGpr = pPr->m_pParent; // 조부모 (p Grand parent)
 
-    pPr->m_pChild[0] = pGpr; // 부모의 왼쪽 자식을 조부모로 변경
-    pGpr->m_pParent = pPr; // 조부모의 부모를 왼쪽 자식으로 변경, 사실상 위랑 세트
+    node->m_pChild[0] = pPr; // 노드의 왼쪽 자식을 부모로 변경
+    pPr->m_pParent = node; // 부모의 부모를 노드로 변경, 사실상 위랑 세트
 
-    pPr->m_pParent = pGGpr; 
-    if (pGpr == pGGpr->m_pChild[0]) pGGpr->m_pChild[0] = pPr;
-    else if (pGpr == pGGpr->m_pChild[1]) pGGpr->m_pChild[1] = pPr;
+    pPr->m_pChild[1] = pLc; // 내려온 pPr의 오른쪽 자식을 떨어져나간 노드의 왼쪽 자식으로 변경
+    if (pLc != nullptr) pLc->m_pParent = pPr; // 위랑 세트
 
-    pPrLc
-
+    node->m_pParent = pGpr; // 노드의 부모를 조부모로 변경
+    if (pGpr != nullptr)
+    {
+        if (pPr == pGpr->m_pChild[0]) pGpr->m_pChild[0] = node; // 부모가 조부모의 왼쪽이었다면 조부모의 왼쪽 자식을 노드로 변경
+        else if (pPr == pGpr->m_pChild[1]) pGpr->m_pChild[1] = node; // 부모가 조부모의 오른쪽이었다면 조부모의 오른쪽 자식을 노드로 변경
+    } // 얘도 위에 한줄이랑 세트
+    else // 노드의 부모가 없다면, 노드가 루트노드라는 뜻
+    {
+        g_pRoot = node;
+    }
 }
 
-void FixRBT(Node* ptr) 
+void RightRotate(Node* node)
+{
+    Node* pPr = node->m_pParent;
+
+    //앞으로 연결이 끊길 노드들을 미리 세이브해둠
+    Node* pRc = node->m_pChild[1]; // 노드의 원래 오른쪽 자식 (p Right chid)
+    Node* pGpr = pPr->m_pParent; // 조부모 (p Grand parent)
+
+    node->m_pChild[1] = pPr; // 노드의 오른쪽 자식을 부모로 변경
+    pPr->m_pParent = node; // 부모의 부모를 노드로 변경, 사실상 위랑 세트
+
+    pPr->m_pChild[0] = pRc; // 내려온 pPr의 왼쪽 자식을 떨어져나간 노드의 오른쪽 자식으로 변경
+    if (pRc != nullptr)pRc->m_pParent = pPr; // 위랑 세트
+
+    node->m_pParent = pGpr; // 노드의 부모를 조부모로 변경
+    if (pGpr != nullptr)
+    {
+        if (pPr == pGpr->m_pChild[0]) pGpr->m_pChild[0] = node; // 부모가 조부모의 왼쪽이었다면 조부모의 왼쪽 자식을 노드로 변경
+        else if (pPr == pGpr->m_pChild[1]) pGpr->m_pChild[1] = node; // 부모가 조부모의 오른쪽이었다면 조부모의 오른쪽 자식을 노드로 변경
+    } // 얘도 위에 한줄이랑 세트
+    else // 노드의 부모가 없다면, 노드가 루트노드라는 뜻
+    {
+        g_pRoot = node;
+    }
+}
+
+void FixRBT(Node* ptr)
 {
     Node* pParent = nullptr;
     Node* pGrandParent = nullptr;
@@ -84,43 +113,99 @@ void FixRBT(Node* ptr)
         if (pParent == pGrandParent->m_pChild[0]) // 부모가 조부모의 왼쪽 자식일 때 
         {
             pUncle = pGrandParent->m_pChild[1]; // 삼촌은 조부모의 오른쪽 자식, 삼촌이 없을수도 있음
-            if (GetColor(pUncle) == RED) 
+            if (GetColor(pUncle) == RED)
             {
                 pParent->m_iColor = BLACK;
                 pUncle->m_iColor = BLACK;
                 pGrandParent->m_iColor = RED;
                 ptr = pGrandParent;
             }
-            else if (GetColor(pUncle) == BLACK) 
+            else if (GetColor(pUncle) == BLACK)
             {
-                if (ptr == pParent->m_pChild[0]) 
+                if (ptr == pParent->m_pChild[0])
                 {
-                    RightRotate(ptr); // 음..
-                    ptr = pGrandParent; // 
+                    RightRotate(ptr->m_pParent);
+                    pGrandParent->m_iColor = RED;
+                    pParent->m_iColor = BLACK; // 쌤은 스왑으로 처리하신 부분
+                    ptr = pParent;
                 }
-                else if (ptr == pParent->m_pChild[1]) 
+                else if (ptr == pParent->m_pChild[1])
                 {
-                    LeftRotate(ptr); 
+                    LeftRotate(ptr);
                     RightRotate(ptr);
-                    ptr = pGrandParent;
+                    pGrandParent->m_iColor = RED;
+                    ptr->m_iColor = BLACK;
                 }
             }
         }
         else if (pParent == pGrandParent->m_pChild[1]) // 부모가 조부모의 오른쪽 자식일 때, else 써도 되는데 보기 편하라고
         {
+            pUncle = pGrandParent->m_pChild[0]; // 삼촌은 조부모의 왼쪽 자식, 삼촌이 없을수도 있음
+            if (GetColor(pUncle) == RED)
+            {
+                pParent->m_iColor = BLACK;
+                pUncle->m_iColor = BLACK;
+                pGrandParent->m_iColor = RED;
+                ptr = pGrandParent;
+            }
+            else if (GetColor(pUncle) == BLACK)
+            {
+                if (ptr == pParent->m_pChild[0])
+                {
+                    RightRotate(ptr);
+                    LeftRotate(ptr);
+                    pGrandParent->m_iColor = RED;
+                    ptr->m_iColor = BLACK;
+                }
+                else if (ptr == pParent->m_pChild[1])
+                {
+                    LeftRotate(ptr->m_pParent);
+                    pGrandParent->m_iColor = RED;
+                    pParent->m_iColor = BLACK; // 쌤은 스왑으로 처리하신 부분
+                    ptr = pParent;
+                }
+            }
         }
     }
     g_pRoot->m_iColor = BLACK;
 }
+
+Node* Search_BST(Node* pParent, int iValue)
+{
+    if (pParent == nullptr)
+    {
+        std::cout << "찾는 숫자가 없음";
+        return nullptr;
+    }
+
+    if (pParent->m_iData == iValue)  return pParent;
+    else if (iValue < pParent->m_iData)
+    {
+        Search_BST(pParent->m_pChild[0], iValue);
+    }
+    else
+    {
+        Search_BST(pParent->m_pChild[1], iValue);
+    }
+}
+
 
 
 
 int main()
 {
     Node* root = nullptr;
+    root = AddNode_BST(root);
+    g_pRoot = root;
+    FixRBT(Search_BST(root, g_iData[g_iCount - 1]));
 
-    while (g_iCount < 10) { root = AddNode_BST(root); }
+    while (g_iCount < 10)
+    {
+        root = AddNode_BST(g_pRoot);
+        FixRBT(Search_BST(root, g_iData[g_iCount - 1]));
+    }
 
+    std::cout << "Hello, World!";
     std::cout << "Hello, World!";
 
 }
